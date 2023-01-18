6f36c9a646dbee9aa2200eb2d8ad316c8476f85a
Kristina <kristinaer304@gmail.com>
2023-01-18 07:11:52 +0200

    Paddle commit

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Nikita's the best")
+import pgzrun
+import pygame
+
+from Paddle import Paddle
+
+
+WIDTH = 700
+HEIGHT = 400
+window = pygame.display.set_mode((WIDTH,HEIGHT))
+paddle = Paddle(window)
+
+windowRect = window.get_rect()
+
+def on_mouse_move(pos): # функция управления платформой с помощью курсора
+    x, y = pos
+    paddle.rect.x = x
+    if (paddle.rect.right >= window.get_rect().right):
+        paddle.rect.right = window.get_rect().right
+    if (paddle.rect.left <= window.get_rect().left):
+        paddle.rect.left = window.get_rect().left
+
+
+pgzrun.go()
