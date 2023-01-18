ce6a77c0a9c417f943423cba231f13ee30491890
Kristina <kristinaer304@gmail.com>
2023-01-18 07:19:27 +0200

    Ball commit

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import pgzrun
 import pygame
 
 from Paddle import Paddle
-
+from Ball import Ball
 
 WIDTH = 700
 HEIGHT = 400
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 paddle = Paddle(window)
-
+ball = Ball(window)
 windowRect = window.get_rect()
 
 def on_mouse_move(pos): # функция управления платформой с помощью курсора
